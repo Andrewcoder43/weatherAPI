f7f2be3dbbe0e131cb9e531f8082ab9e5eccd9f3
Scott Cormier <chicormier@gmail.com>
2023-10-16 16:56:50 -0500

    initial push

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const btn = document.querySelector('button');
 const output = document.querySelector('#output');
-const intake = document.querySelector('input');
+const intake = document.querySelector('#intake');
 const data = null;
 const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=';
 
@@ -12,25 +12,23 @@ function getInput() {
   console.log(tempVal);
   //the url makes a request from API based on that last string value added to the url
   let tempURL = url + tempVal;
+  //clear previous results
+  output.innerHTML = '';
   xhr.onload = function () {
-    if (xhr.readyState === 4 && xhr.status == '200') {
-      let data = JSON.parse(xhr.responseText);
-      //this changes the 'data' into a JSON format that should be easier to loop through once we figure it out.
-      console.log(data);
-      // outputHTML(data);
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        //change the 'data' into a JSON format that should be easier to loop through once we figure it out.
+        let data = JSON.parse(xhr.responseText);
+        outputHTML(data);
+      } else {
+        // handle api request errors here
+        output.innerHTML = 'error fetching data from the api';
+      }
     }
   };
   // sends the GET request with the added tempVal from submission field
   xhr.open('GET', tempURL);
-
   xhr.withCredentials = true;
-
-  xhr.addEventListener('readystatechange', function () {
-    if (this.readyState === this.DONE) {
-      // we need to loop through this info, I think, so that only what we want is displayed
-      output.innerHTML += this.responseText;
-    }
-  });
 
   xhr.setRequestHeader(
     'X-RapidAPI-Key',
@@ -38,9 +36,17 @@ function getInput() {
   );
   xhr.setRequestHeader('X-RapidAPI-Host', 'weatherapi-com.p.rapidapi.com');
 
-  xhr.send(data);
-  //   console.log(intake.value);
+  xhr.send();
 }
+
+// xhr.addEventListener('readystatechange', function () {
+//   if (this.readyState === this.DONE) {
+//     // we need to loop through this info, I think, so that only what we want is displayed
+//     output.innerHTML += this.responseText;
+//   }
+// });
+
+//   console.log(intake.value);
 
 // function outputHTML(data) {
 //   console.log(data);
